aec546b1fcb0393765651afcda7a7cd9f8caaf25
Coder <111493707+informationabout@users.noreply.github.com>
2024-05-27 13:42:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("fname").addEventListener("blur", function() {
+        this.value = this.value.toUpperCase();
+    });
+});
